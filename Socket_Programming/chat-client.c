08f92fabc53fe2244d13284a3060fedec5679c08
Soumya-2003 <soumyadeepmallicksci@gmail.com>
2025-03-07 21:57:47 +0530

    Add TCP chat client implementation in chat-client.c

---
+++ Socket_Programming/chat-client.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+
+int main(){
+    int sd, cad1;
+    struct sockaddr_in sad, cad;
+    char str[50];
+
+    sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    sad.sin_family = AF_INET;
+    sad.sin_port = htons(8000);
+    sad.sin_addr.s_addr = inet_addr("192.168.0.111");
+
+    connect(sd, (struct sockaddr *)&sad, sizeof(sad));
+
+    do{
+        recv(sd, str, sizeof(str), 0);
+        printf("\nCodeword received from sender: %s\n", str);
+        printf("Enter the string: ");
+        fflush(stdin);
+        scanf("%s", str);
+        send(sd, str, sizeof(str), 0);
+    } while (strcmp(str, "bye") != 0);
+    
+    close(sd);
+}
